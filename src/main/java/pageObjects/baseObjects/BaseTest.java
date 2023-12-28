b389c9a4c5eaa6e8229ce25a90515be3a5583c3e
Julia_Pimakhova <yuliitkach@gmail.com>
2023-12-28 15:17:50 +0300

    Add lesson 11 Allure screenshots

--- src/main/java/pageObjects/baseObjects/BaseTest.java
+++ src/main/java/pageObjects/baseObjects/BaseTest.java
@@ -3,6 +3,7 @@ package pageObjects.baseObjects;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
+import testngUtils.InvokedListener;
 import testngUtils.Listener;
 
 import static driver.DriverType.CHROME;
@@ -10,9 +11,9 @@ import static driver.DriverType.valueOf;
 import static driver.ImprovedWebDriver.createDriver;
 import static driver.ImprovedWebDriver.quitDriver;
 import static propertyUtils.PropertyReader.getProperties;
-@Listeners(Listener.class)
-public class BaseTest {
 
+@Listeners({Listener.class, InvokedListener.class})
+public class BaseTest {
     @BeforeTest
     public void startUp() {
         createDriver(System.getProperties().containsKey("config")
